4961c5712375ca61e665dfa23624a6e4865bc509
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-11-19 17:04:53 -0600

    inital code

---
+++ src/main/java/com/springsecuritywithoauthclient/SpringSecurityWithOauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritywithoauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithOauthClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecuritywithoauthclient/SpringSecurityWithOauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecuritywithoauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithOauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
